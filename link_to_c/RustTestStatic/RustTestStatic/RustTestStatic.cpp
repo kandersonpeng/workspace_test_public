3dc94f3fa91017d6b1d46747cd7d8a1af539be66
Kevin Anderson <kanderson.home@proton.me>
2023-03-03 15:16:02 -0400

    Added C++ allocation with string.  Rust release works, debug fails.

--- link_to_c/RustTestStatic/RustTestStatic/RustTestStatic.cpp
+++ link_to_c/RustTestStatic/RustTestStatic/RustTestStatic.cpp
@@ -5,10 +5,17 @@
 #include "framework.h"
 #include "RustTestStatic.h"
 #include <cstdio>
+#include <string>
 
 
 size_t print_int_val(int value)
 {
+#ifdef _DEBUG
+    std::string profile = "DEBUG";
+#else
+    std::string profile = "RELEASE";
+#endif
+    printf("Profile is: %s\n", profile.c_str());
     auto num_printed = printf("Value is: %d\n", value);
     return num_printed;
 }
